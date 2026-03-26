9a8cbb1622ffb42c3999a88f245b080ad6631028
d15944343-a11y <d15944343@gmail.com>
2026-03-26 21:49:25 +0300

    Добавил отчет по городам в аналитике

--- app/routes.py
+++ app/routes.py
@@ -61,6 +61,10 @@ def analytics():
     for client in clients:
         status_counts[client.status] = status_counts.get(client.status, 0) + 1
 
+    city_counts = {}
+    for client in clients:
+        city_counts[client.city] = city_counts.get(client.city, 0) + 1
+
     return render_template(
         "analytics.html",
         total_clients=total_clients,
@@ -68,6 +72,7 @@ def analytics():
         inactive_clients=inactive_clients,
         unique_cities=unique_cities,
         status_counts=status_counts,
+        city_counts=city_counts,
         breadcrumbs=[
             {"title": "Главная", "endpoint": "main.index"},
             {"title": "Аналитика"},
