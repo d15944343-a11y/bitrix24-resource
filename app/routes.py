1124979ce0acbf0b597e67091c80744ba760dfd0
d15944343-a11y <d15944343@gmail.com>
2026-03-26 21:05:41 +0300

    Добавить базовую навигацию и публичные страницы

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,29 @@ main_bp = Blueprint("main", __name__)
 
 @main_bp.route("/")
 def index():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        breadcrumbs=[{"title": "Главная"}],
+    )
+
+
+@main_bp.route("/about")
+def about():
+    return render_template(
+        "about.html",
+        breadcrumbs=[
+            {"title": "Главная", "endpoint": "main.index"},
+            {"title": "О проекте"},
+        ],
+    )
+
+
+@main_bp.route("/contacts")
+def contacts():
+    return render_template(
+        "contacts.html",
+        breadcrumbs=[
+            {"title": "Главная", "endpoint": "main.index"},
+            {"title": "Контакты"},
+        ],
+    )
