75eac77e55f82ef689f05bf22ab1e88e3894527b
d15944343-a11y <d15944343@gmail.com>
2026-04-08 13:04:25 +0300

    Собрал основу

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
-    APP_NAME = "Bitrix24 Resource"
+    APP_NAME = "Bitrix24 AI Resource"
     APP_HOST = os.getenv("FLASK_RUN_HOST", "127.0.0.1")
     APP_PORT = int(os.getenv("FLASK_RUN_PORT", "5000"))
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///site.db")
